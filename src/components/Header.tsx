e2eb03b66598322abd2aeb8c0afb2b84af910ad4
Philip William Ventura <philipwilliamventura@gmail.com>
2025-04-24 15:25:06 -0400

    setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+const Header = () => {
+
+  const [nav, setNav] = useState(false)
+
+  return (
+    <header>
+        <nav>
+            
+        </nav>
+    </header>
+  )
+}
+
+export default Header
